430534ab0c530c986beda0e02d58e9223a094240
THANETNove <Novel_Manga@outlook.co.th>
2023-06-23 18:35:09 +0700

    uocpd

--- screens/Home.js
+++ screens/Home.js
@@ -1,55 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
-import Contacts from 'react-native-contacts';
-import { check, PERMISSIONS, request } from 'react-native-permissions';
 
-const PhoneNumberList = () => {
-    const [phoneContacts, setPhoneContacts] = useState([]);
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+export default class Home extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Open up App. Home app!</Text>
 
-    useEffect(() => {
-        fetchPhoneContacts();
-    }, []);
+            </View>
+        )
+    }
+}
 
-    const fetchPhoneContacts = async () => {
-        try {
-            /*  const permissionStatus = await check(PERMISSIONS.ANDROID.READ_CONTACTS);
-             if (permissionStatus === 'granted') {
-                 Contacts.getAll().then((contacts) => {
-                     setPhoneContacts(contacts);
-                 });
-             } else {
-                 const permissionResponse = await request(
-                     PERMISSIONS.ANDROID.READ_CONTACTS
-                 );
-                 if (permissionResponse === 'granted') {
-                     Contacts.getAll().then((contacts) => {
-                         setPhoneContacts(contacts);
-                     });
-                 } else {
-                     console.log('Permission denied');
-                 }
-             } */
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    /*   const renderPhoneContact = ({ item }) => (
-          <View style={{ padding: 10 }}>
-              <Text>Name: {item.displayName}</Text>
-              <Text>Phone: {item.phoneNumbers[0].number}</Text>
-          </View>
-      ); */
-
-    return (
-        <View>
-            {/*    <FlatList
-                data={phoneContacts}
-                renderItem={renderPhoneContact}
-                keyExtractor={(item) => item.recordID}
-            /> */}
-        </View>
-    );
-};
-
-export default PhoneNumberList;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
